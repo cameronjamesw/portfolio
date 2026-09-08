25bc42f39d4c5fdf845d0835c740ddb781b310f7
Cameron Wheatley <cameron_2000@live.co.uk>
2026-09-08 13:14:35 -0400

    create certification model

--- certificates/models.py
+++ certificates/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from projects.models import Skill
 
 # Create your models here.
+
+class Certification(models.Model):
+
+    name = models.CharField(max_length=100)
+
+    provider = models.CharField(max_length=100)
+
+    date_completed = models.DateField()
+
+    expiry_date = models.DateField(
+        blank=True,
+        null=True
+    )
+
+    credential_url = models.URLField(
+        blank=True
+    )
+
+    skills = models.ManyToManyField(
+        Skill,
+        blank=True
+    )
+
+    def __str__(self):
+        return self.name
