c2b1fe3cb001e3fcb49f9515e78d71721baa96be
Cameron Wheatley <cameron_2000@live.co.uk>
2026-09-09 15:34:03 -0400

    update template file structure

--- about/views.py
+++ about/views.py
@@ -4,13 +4,13 @@ from django.http import HttpResponse
 # Create your views here.
 
 def about_page(request):
-    return render(request, 'about.html')
+    return render(request, 'about/about.html')
 
 def certificates_url(request):
     return HttpResponse("Welcome to the certificates page!")
 
 def educaton_url(request):
-    return render(request, 'education.html')
+    return render(request, 'about/education.html')
 
 def exp_page(request):
-    return render(request, 'experience.html')
+    return render(request, 'about/experience.html')
