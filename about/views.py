10427cb2309816f18e18b7e0bbb86577a8367b72
Cameron Wheatley <cameron_2000@live.co.uk>
2026-09-09 11:19:20 -0400

    create about app, delete certificate app, make migrations

--- about/views.py
+++ about/views.py
@@ -5,3 +5,4 @@ from django.http import HttpResponse
 
 def certificates_url(request):
     return HttpResponse("Welcome to the certificates page!")
+
--- my_portfolio/urls.py
+++ my_portfolio/urls.py
@@ -21,5 +21,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include("home.urls")),
     path('', include("projects.urls")),
-    path('', include("certificates.urls")),
+    path('', include("about.urls")),
 ]
