52d5d6e5f5d56e09f3b0f8b5cac49e28c0f4c5bc
Cameron Wheatley <cameron_2000@live.co.uk>
2026-09-09 12:39:22 -0400

    fix urls

--- my_portfolio/settings.py
+++ my_portfolio/settings.py
@@ -29,9 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost',
-                 'git.heroku.com/my-portfolio-site.git',
-                 'my-portfolio-site-18dba687e2d2.herokuapp.com',
-                 '.herokuapp.com']
+                 'my-portfolio-site-18dba687e2d2.herokuapp.com']
 
 
 # Application definition
--- my_portfolio/urls.py
+++ my_portfolio/urls.py
@@ -20,6 +20,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include("home.urls")),
-    path('', include("projects.urls")),
+    path('projects/', include("projects.urls")),
     path('about/', include("about.urls")),
 ]
