ff5adcd8967070efca86d463379418c608f6ab2f
Cameron Wheatley <cameron_2000@live.co.uk>
2026-09-09 17:55:06 -0400

    add queryset to education views

--- about/views.py
+++ about/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Education
 
 # Create your views here.
 
@@ -10,7 +11,13 @@ def certificates_url(request):
     return HttpResponse("Welcome to the certificates page!")
 
 def educaton_url(request):
-    return render(request, 'about/education.html')
+    education_list = Education.objects.all().order_by('-graduation_date')
+
+    context = {
+        "education_list": education_list
+    }
+
+    return render(request, 'about/education.html', context)
 
 def exp_page(request):
     return render(request, 'about/experience.html')
