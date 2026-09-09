e9696f9bfd70f6b88a65718b1144fd6ad44f3173
Cameron Wheatley <cameron_2000@live.co.uk>
2026-09-09 12:14:01 -0400

    create views and url

--- about/urls.py
+++ about/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.about_page, name="about"),
     path('certifications/', views.certificates_url, name="certifications"),
+    path('education/', views.educaton_url, name="education"),
+    path('experience/', views.exp_page, name="experience"),
 ]
--- about/views.py
+++ about/views.py
@@ -3,6 +3,14 @@ from django.http import HttpResponse
 
 # Create your views here.
 
+def about_page(request):
+    return HttpResponse("About Page")
+
 def certificates_url(request):
     return HttpResponse("Welcome to the certificates page!")
 
+def educaton_url(request):
+    return HttpResponse("Education Page")
+
+def exp_page(request):
+    return HttpResponse("Experience Page")
