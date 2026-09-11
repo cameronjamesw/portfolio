45484bf1051d5a3a3e0674b63f290b9f7adaea0e
Cameron Wheatley <cameron_2000@live.co.uk>
2026-09-11 14:20:24 -0400

    update image field in projects model

--- projects/models.py
+++ projects/models.py
@@ -1,5 +1,4 @@
 from django.db import models
-from cloudinary.models import CloudinaryField
 
 # Create your models here.
 
@@ -8,7 +7,7 @@ class Project(models.Model):
     description = models.TextField(blank=True)
     github_url = models.URLField(blank=True)
     live_url = models.URLField(blank=True)
-    image = CloudinaryField("image", blank=True)
+    image = models.ImageField(upload_to='images/', blank=True)
     start_date = models.DateField()
     completion_date = models.DateField(blank=True, null=True)
 
