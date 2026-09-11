50c13656b74ef0eeff233fbb29127315ad5af3c5
Cameron Wheatley <cameron_2000@live.co.uk>
2026-09-11 14:03:03 -0400

    create projects.html and projects.css

--- projects/views.py
+++ projects/views.py
@@ -1,7 +1,20 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from .models import Project, Technology
 
 # Create your views here.
 
-def project_url(index):
-    return HttpResponse('This is the projects URL!!')
+def project_list_url(request):
+    technology_id = request.GET.get('technology')
+
+    project_list = Project.objects.all().prefetch_related('technologies', 'skills').order_by('-start_date')
+
+    if technology_id:
+        project_list = project_list.filter(technologies__id=technology_id)
+
+    technologies = Technology.objects.all().order_by('category', 'name')
+
+    return render(request, 'projects/projects.html', {
+        'project_list': project_list,
+        'technologies': technologies,
+        'selected_technology': technology_id,
+    })
