9061abbcee3f36d1f91540a47fd5abbf1f768840
Cameron Wheatley <cameron_2000@live.co.uk>
2026-09-15 12:31:19 -0400

    add language to admin panel

--- projects/admin.py
+++ projects/admin.py
@@ -1,10 +1,12 @@
 from django.contrib import admin
-from .models import Project, Technology, Skill
+from .models import Project, Technology, Skill, Language
 
 # Register your models here.
 
 admin.site.register(Project)
 
 admin.site.register(Technology)
+
+admin.site.register(Language)
 
 admin.site.register(Skill)
